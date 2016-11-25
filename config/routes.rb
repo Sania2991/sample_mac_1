e8e029f8a768cf67639ff68eed8d4154a2af75ad
Sania2991 <sania2991@rambler.ru>
2016-11-25 11:03:34 +0300

    finished Chepter_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  # p_195
+  get '/signup',  to: 'users#new'
+  # p_189 ...
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  # ... p_189
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require "minitest/reporters"                                                    # p_119 ...
-Minitest::Reporters.use!                                                        # ... p_119
+require "minitest/reporters"                                                     # p_119 ...
+Minitest::Reporters.use!                                                         # ... p_119
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+ include ApplicationHelper                                                       # P_198
   # Add more helper methods to be used by all tests here...
 end
