a171cb3be8d03d718e422e71f86c197a56b8ce61
Sania2991 <sania2991@rambler.ru>
2016-11-30 15:38:07 +0300

    Ooops

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -25,8 +25,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       # post_via_redirect - передача post запроса маршруту: users_path
       post_via_redirect users_path, user: { name:  "Example User",
-                                            email: "user@example.com"
-                                            password:              "password"
+                                            email: "user@example.com",
+                                            password:              "password",
                                             password_confirmation: "password"}
     end
     assert_template 'users/show'
