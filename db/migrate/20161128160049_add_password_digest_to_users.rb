9faea05915e698f8ddd1d71d500b12464ce69361
Sania2991 <sania2991@rambler.ru>
2017-01-02 19:47:37 +0300

    nill project

--- db/migrate/20161128160049_add_password_digest_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordDigestToUsers < ActiveRecord::Migration[5.0]
-  def change
-    # p_230: добавление в табл. users столбца password_digest
-    add_column :users, :password_digest, :string
-  end
-end
