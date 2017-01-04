b674cbedc711ce5a79946874782615454fd73c87
Sania2991 <sania2991@rambler.ru>
2017-01-04 20:13:45 +0300

    Закончили 9.3.2: Образцы пользователей

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Rake-задача для заполнения Б.Д. образцами пользователей, испльзуя gem 'faker'
+# p_379: этот код дублирует существ. пользователя, а затем создает 99 образцов.
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  # метод create! - действует также, как create, только вызывает искл.
+    # для недопустимого пользователя, вместо возврата false
+  User.create!(name: name,
+               email: email,
+               password:               password,
+               password_confirmation: password)
+end
+
+# p_380:
+# в консоли:
+# 1). После добавления gem 'faker'
+# $ bundle install
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 2). Чтобы выполнить Rake-задачу, нужно сначала очистить Б.Д., а потом вызвать:
+# $ rails db:migrate:reset
+# $ rails db:seed
